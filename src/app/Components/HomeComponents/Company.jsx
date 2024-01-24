a9738dfceef65237eac5c45167addd152df57c99
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-01-24 19:30:28 +0530

    company

---
+++ src/app/Components/HomeComponents/Company.jsx
@@ -0,0 +1,33 @@
+'use client';
+import React from 'react'
+import Image from "next/image";
+import assets from "@/assets/allImages";
+function Company() {
+  const imgData = [
+     assets.microsoft,
+    assets.google,
+     assets.amazon,
+     assets.facebook,
+    assets.netflix,
+     assets.samsung,
+  ]
+  return (
+    <div className="flex flex-wrap bg-grey-bg gap-4 px-8 justify-evenly items-center">
+        {
+          imgData.map((src,i)=>{
+            return(
+              <Image
+                src={src}
+                width={144}
+                alt="microsoft"
+                className="aspect-w-4 aspect-h-1"
+              />
+            )
+          })
+        }
+        
+      </div>
+  )
+}
+
+export default Company
