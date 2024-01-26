c1f7ae4c2b46c6fbd750302654b826529fcfd346
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-01-26 18:15:32 +0530

    interview experiance

--- public/dummyData.js
+++ public/dummyData.js
@@ -4,36 +4,43 @@ const indianCricketers = [
       name: "Ravindra Jadeja",
       imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4vt66t4p4tWqU9WZDW1VpBkvvEpX0deB5aQ&usqp=CAU",
       description: "An all-rounder with exceptional fielding skills and a reliable left-arm spinner.",
+      rating:5,
     },
     {
       name: "Ravindra Jadeja",
       imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4vt66t4p4tWqU9WZDW1VpBkvvEpX0deB5aQ&usqp=CAU",
       description: "An all-rounder with exceptional fielding skills and a reliable left-arm spinner.",
+      rating:4,
     },
     {
       name: "Ravindra Jadeja",
       imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4vt66t4p4tWqU9WZDW1VpBkvvEpX0deB5aQ&usqp=CAU",
       description: "An all-rounder with exceptional fielding skills and a reliable left-arm spinner.",
+      rating:3,
     },
     {
       name: "Ravindra Jadeja",
       imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4vt66t4p4tWqU9WZDW1VpBkvvEpX0deB5aQ&usqp=CAU",
       description: "An all-rounder with exceptional fielding skills and a reliable left-arm spinner.",
+      rating:2,
     },
     {
       name: "Ravindra Jadeja",
       imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4vt66t4p4tWqU9WZDW1VpBkvvEpX0deB5aQ&usqp=CAU",
       description: "An all-rounder with exceptional fielding skills and a reliable left-arm spinner.",
+      rating:3,
     },
     {
       name: "Ravindra Jadeja",
       imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4vt66t4p4tWqU9WZDW1VpBkvvEpX0deB5aQ&usqp=CAU",
       description: "An all-rounder with exceptional fielding skills and a reliable left-arm spinner.",
+      rating:5,
     },
     {
       name: "Ravindra Jadeja",
       imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4vt66t4p4tWqU9WZDW1VpBkvvEpX0deB5aQ&usqp=CAU",
       description: "An all-rounder with exceptional fielding skills and a reliable left-arm spinner.",
+      rating :4,
     },
     ];
     
