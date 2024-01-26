243357b9243dfea7801cb0fdd86746679473e938
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-01-27 02:56:35 +0530

    ham

--- src/app/Components/NavbarComponents/MobileNavbar.jsx
+++ src/app/Components/NavbarComponents/MobileNavbar.jsx
@@ -63,7 +63,7 @@ function MobileNavbar({ navBurger }) {
             setDelay(true);
           }, 500);
         }}
-        className={`hidden hamburger font-semibold z-[99] hover:bg-dark-blue bg-green-bg cursor-pointer transition duration-300  rounded-md p-2 text-white`}
+        className={`hidden hamburger font-semibold z-[45] hover:bg-dark-blue bg-green-bg cursor-pointer transition duration-300  rounded-md p-2 text-white`}
       >
         <FaBars size={15} />
       </div>
