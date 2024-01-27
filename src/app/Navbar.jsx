45f867e081772db10f3620c6f797af3aebebaeed
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-01-27 19:04:57 +0530

    avatar

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,32 +1,36 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useSession } from "next-auth/react";
-
+import { signOut, useSession } from "next-auth/react";
 
 import Dropdown from "./Components/NavbarComponents/Dropdown";
 import Login from "./Components/NavbarComponents/Login";
 import "./globals.css";
-import './Navbar.css';
+import "./Navbar.css";
 import MobileNavbar from "./Components/NavbarComponents/MobileNavbar";
 import { FaBars } from "react-icons/fa";
 import { dropdownData } from "./Components/NavbarComponents/NavbarData";
-import {toggleSignPagePopup,setSignInBtn} from './GlobalRedux/Features/GlobalStateSlice';
+import {
+  toggleSignPagePopup,
+  setSignInBtn,
+} from "./GlobalRedux/Features/GlobalStateSlice";
 import { FaCircleArrowUp } from "react-icons/fa6";
+import Avatar from "react-avatar";
 function Navbar() {
 
-  const { data: sessionData } = useSession();
-  console.log('session ',sessionData);
+  const isLogin = useSelector((state) => {return state.GlobalState.isLogin});
 
-  const isSignup = useSelector((state)=>{return state.GlobalState.isSignup});
+  const isSignup = useSelector((state) => {
+    return state.GlobalState.isSignup;
+  });
 
   const dispatch = useDispatch();
-  const flag = useSelector((state)=>{
+  const flag = useSelector((state) => {
     return state.GlobalState.isSignPagePopup;
-  })
+  });
   let a = "</IE>";
 
-  const [navBurger , setNavBurger] = useState(false);
+  const [navBurger, setNavBurger] = useState(false);
 
   const [showBackToTop, setShowBackToTop] = useState(false);
 
@@ -54,62 +58,65 @@ function Navbar() {
   return (
     <div>
       <div
-          className={`fixed bottom-4 bg-white rounded-full w-fit z-10 right-2 cursor-pointer text-dark-blue hover:text-green-bg transition-all duration-300 back-to-top ${showBackToTop ? 'rotate-in' : 'rotate-out'}`}
-          title="Back to top"
-          onClick={scrollToTop}
-        >
-          <FaCircleArrowUp size={40} />
-        </div>
+        className={`fixed bottom-4 bg-white rounded-full w-fit z-10 right-2 cursor-pointer text-dark-blue hover:text-green-bg transition-all duration-300 back-to-top ${
+          showBackToTop ? "rotate-in" : "rotate-out"
+        }`}
+        title="Back to top"
+        onClick={scrollToTop}
+      >
+        <FaCircleArrowUp size={40} />
+      </div>
       <div className="flex justify-between h-[10vh] items-center">
-      <p className="px-4 py-2 text-yellow-400   font-bold text-3xl">{a}</p>
-
-      {/* Dropdowns */}
-      <div className="gap-7 font-semibold hidden nmd:flex">
-        {dropdownData.map((obj, index) => {
-          return (
-            <Dropdown label={obj.label} options={obj.options} key={index} />
-          );
-        })}
-      </div>
-
-      <div onClick={()=>setNavBurger(!navBurger)} className="nmd:hidden">
-        <FaBars size={25}/>
-      </div>
-
-      <div className="hidden nmd:flex items-center gap-6">
-        <div>
-          <button
-            className="py-2 px-4 start-2  font-semibold rounded-md transition duration-300"
-            onClick={() => {
-              setSignInBtn(false);
-              dispatch(toggleSignPagePopup());
-            }}
-          >
-            Sign In
-          </button>
+        <p className="px-4 py-2 text-yellow-400   font-bold text-3xl">{a}</p>
+        {/* Dropdowns */}
+        <div className="gap-7 font-semibold hidden nmd:flex">
+          {dropdownData.map((obj, index) => {
+            return (
+              <Dropdown label={obj.label} options={obj.options} key={index} />
+            );
+          })}
         </div>
-        <button
-          className="py-2 px-4 text-white font-semibold rounded-lg btn-gradient-2 hidden sm:flex"
-          onClick={() => {
-            setSignInBtn(true);
-            dispatch(toggleSignPagePopup());
-          }}
-        >
-          Sign Up
-        </button>
-      </div>
-      {flag && (
-        <Login
-          isSignup={isSignup}
-          setSignInBtn={setSignInBtn}
-          
-        ></Login>
-      )}
 
+        <div onClick={() => setNavBurger(!navBurger)} className="nmd:hidden">
+          <FaBars size={25} />
+        </div>
+
+        {!isLogin ? (
+          <div className="hidden nmd:flex items-center gap-6">
+            <div>
+              <button
+                className="py-2 px-4 start-2  font-semibold rounded-md transition duration-300"
+                onClick={() => {
+                  setSignInBtn(false);
+                  dispatch(toggleSignPagePopup());
+                }}
+              >
+                Sign In
+              </button>
+            </div>
+            <button
+              className="py-2 px-4 text-white font-semibold rounded-lg btn-gradient-2 hidden sm:flex"
+              onClick={() => {
+                setSignInBtn(true);
+                dispatch(toggleSignPagePopup());
+              }}
+            >
+              Sign Up
+            </button>
+          </div>) : (<div className="hidden nmd:flex" onClick={signOut}>
+            <Avatar
+              name="Wim Mostmans"
+              src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+              size="50"
+              round={true}
+            ></Avatar>
+          </div>)
+        }
+
+        {flag && <Login></Login>}
+      </div>
+      <MobileNavbar navBurger={navBurger} setNavBurger={setNavBurger} />
     </div>
-      <MobileNavbar navBurger={navBurger} setNavBurger={setNavBurger}/>
-    </div>
-  
   );
 }
 
