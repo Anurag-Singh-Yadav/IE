1bb9a28844fc23e5c87cb3183dd5d27e9e5dbdb1
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-02-03 17:04:03 +0530

    overflow

--- src/app/Components/HomeComponents/Hero.jsx
+++ src/app/Components/HomeComponents/Hero.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { FaArrowRight } from "react-icons/fa6";
 function Hero() {
   return (
-    <div>
+    <div className="overflow-hidden">
       <div className="fixed top-0 right-0 pt-4 h-[100vh] w-[100vw] flex justify-center items-center  background-grid -z-10">
         <div
           id="hero"
