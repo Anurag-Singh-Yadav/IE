bb41e20d85eb6e85a1b405a23d7a712f7ddd472c
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-02-09 22:18:22 +0530

    routing

--- src/app/Components/Navigator.jsx
+++ src/app/Components/Navigator.jsx
@@ -1,12 +1,18 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
 import "./navigator.css";
 import { FaCircle } from "react-icons/fa";
 import Link from "next/link";
-function Navigator({ navigator,setNavBarClick,navBarClick }) {
-  const [activeBar, setActiveBar] = useState(-1);
-  const [activeSubTopics, setActiveSubTopics] = useState(0);
+function Navigator({
+  navigator,
+  setNavBarClick,
+  navBarClick,
+  activeBar,
+  activeSubTopics,
+}) {
+  // const [activeBar, setActiveBar] = useState(-1);
+  // const [activeSubTopics, setActiveSubTopics] = useState(0);
   const { menu } = navigator;
 
   return (
@@ -20,51 +26,50 @@ function Navigator({ navigator,setNavBarClick,navBarClick }) {
             <div key={index} className="sm:py-7 md:py-10">
               <div
                 className={`flex items-center justify-between text-sm hover:bg-green-bg py-2 rounded-sm transition-all duration-300 hover:text-white hover:cursor-pointer font-semibold px-4 ${
-                  activeBar === index
+                  activeBar == item.mainHeading
                     ? "bg-green-bg text-white"
                     : "bg-light-green"
                 }`}
-                onClick={() => {
-                  if (activeBar === index) setActiveBar(-1);
-                  else setActiveBar(index);
-                  setActiveSubTopics(0);
-                }}
               >
                 <span>{item.mainHeading}</span>
                 <FaChevronDown />
               </div>
               <div
                 className={`${
-                  activeBar === index ? "drop-downanimation-add" : "hidden"
+                    "drop-downanimation-add"
                 } px-8`}
               >
                 <div className="flex flex-col">
-                {item.articles.map((subItem, i) => {
-                  return (
-                    <div
-                      key={i}
-                      className="flex items-center border-l-2 border-green-bg"
-                    >
-                      <div className="h-[2px] w-[18px] bg-green-bg flex "></div>
-                      {activeBar == index && activeSubTopics === i && (
-                        <FaCircle className="text-green-bg" />
-                      )}
-                      {activeBar == index && activeSubTopics !== i && (
-                        <FaCircle className="text-white" />
-                      )}
-                      <Link
-                        className="py-3 text-sm px-2 font-medium hover:cursor-pointer"
-                        onClick={() => {
-                          setActiveSubTopics(i);
-                          setNavBarClick(!navBarClick);
-                        }}
-                        href={'/learn/[mainTopic]'} as={`/learn/${navigator.mainTopic}?mainTopic=${navigator.mainTopic}&mainHeading=${item.mainHeading}&title=${subItem}`}
+                  {item.articles.map((subItem, i) => {
+                    return (
+                      <div
+                        key={i}
+                        className="flex items-center border-l-2 border-green-bg"
                       >
-                        {subItem}
-                      </Link>
-                    </div>
-                  );
-                })}
+                        <div className="h-[2px] w-[18px] bg-green-bg flex "></div>
+                        {activeBar == item.mainHeading &&
+                          activeSubTopics == subItem && (
+                            <FaCircle className="text-green-bg" />
+                          )}
+
+                        {activeBar == item.mainHeading &&
+                          activeSubTopics != subItem && (
+                            <FaCircle className="text-white" />
+                          )}
+
+                        <Link
+                          className="py-3 text-sm px-2 font-medium hover:cursor-pointer"
+                          href={"/learn/[mainTopic]"}
+                          as={`/learn/${navigator.mainTopic}?mainTopic=${navigator.mainTopic}&mainHeading=${item.mainHeading}&title=${subItem}`}
+                          onClick={()=>{
+                            setNavBarClick(!navBarClick)
+                          }}
+                        >
+                          {subItem}
+                        </Link>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             </div>
