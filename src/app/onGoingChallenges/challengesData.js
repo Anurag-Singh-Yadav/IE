1b97ca1961ddc37a87c73d959c98d958e6d65b46
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-02-10 22:21:47 +0530

    question ren

--- src/app/onGoingChallenges/challengesData.js
+++ src/app/onGoingChallenges/challengesData.js
@@ -1,33 +1,40 @@
 const challengesData = [
   {
-    id: "1",
-    title: "30 Days Sheets",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam ultricies, nunc nibh aliquet enim, vitae aliquam nisl nunc vitae mauris. ",
-    image: "https://picsum.photos/200/300",
-    topics: ["Array", "stacks", "sorting", "tress"],
-    type: ["Easy", "Medium", "Hard"],
-    duration: "30 Days",
-    questions: [
-      {
-        easy: "url",
-      },
-      {
-        medium: "url",
-      },
-      {
-        hard: "url",
-      },
-      {
-        easy: "url",
-      },
-      {
-        medium: "url",
-      },
-      {
-        hard: "url",
-      },
-    ],
+    "_id": "65c73397de5fadcc09226038",
+    "description": "my first challenges",
+    "challengeType": "30 days DSA challenges",
+    "imageUrl": "https://media.licdn.com/dms/image/D5622AQFC9lTudBdURA/feedshare-shrink_800/0/1696956894498?e=2147483647&v=beta&t=tboo2s5oHZbj7I_gS6nmIFvV9kPTyquZIzhmJNqtkrQ"
   },
+  {
+    "_id": "65c73397de5fadcc09226039",
+    "description": "my second challenges",
+    "challengeType": "Weekly coding challenges",
+    "imageUrl": "https://example.com/image2.jpg"
+  },
+  {
+    "_id": "65c73397de5fadcc09226040",
+    "description": "my third challenges",
+    "challengeType": "Algorithmic puzzles",
+    "imageUrl": "https://example.com/image3.jpg"
+  },
+  {
+    "_id": "65c73397de5fadcc09226041",
+    "description": "my fourth challenges",
+    "challengeType": "Data structures challenges",
+    "imageUrl": "https://example.com/image4.jpg"
+  },
+  {
+    "_id": "65c73397de5fadcc09226042",
+    "description": "my fifth challenges",
+    "challengeType": "Dynamic programming challenges",
+    "imageUrl": "https://example.com/image5.jpg"
+  },
+  {
+    "_id": "65c73397de5fadcc09226043",
+    "description": "my sixth challenges",
+    "challengeType": "Coding interview challenges",
+    "imageUrl": "https://example.com/image6.jpg"
+  }
 ];
+
 export default challengesData;
--- src/app/onGoingChallenges/page.js
+++ src/app/onGoingChallenges/page.js
@@ -1,20 +1,47 @@
-import React from 'react'
-import WebsiteBanner from '../Components/templets/WebsiteBanner'
-
+import React from "react";
+import WebsiteBanner from "../Components/templets/WebsiteBanner";
+import challengesData from "./challengesData.js";
+import Link from "next/link";
 function page() {
   return (
-    <div>
-        <div>
-            <WebsiteBanner
-                imgSrc={'onGoingChallenges.gif'}
-                BtnName={"Accepts Challenges"}
-            ></WebsiteBanner>
+    <div className="min-h-[100vh]">
+      <div>
+        <WebsiteBanner
+          imgSrc={"onGoingChallenges.gif"}
+          BtnName={"Accepts Challenges"}
+        ></WebsiteBanner>
+      </div>
+      <div className="main-container">
+        <div className="font-semibold sm:py-4 md:py-8 text-lg sm:text-xl md:text-2xl lg:text-3xl">
+          Explore Top{" "}
+          <span className="text-green-bg underline">Challenges</span>
         </div>
 
-        
-
+        <div className="grid pb-8 grid-col-1 md:grid-cols-2 gap-4">
+          {challengesData &&
+            challengesData.map((challenges, index) => (
+              <Link
+                key={index}
+                href={{
+                  pathname: `/learn/${challenges._id}`,
+                }}
+              >
+                <div className="flex text-xs sm:text-sm md:text-normal  items-center border-2 border-white hover:border-l-green-bg hover:cursor-pointer p-4 transition-all duration-300 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+                  <img
+                    src={challenges.challenges}
+                    alt={challenges.challengeType}
+                    className="mr-4 w-24 aspect-auto"
+                  />
+                  <div>
+                    <div className="font-bold">{challenges.challengeType}</div>
+                  </div>
+                </div>
+              </Link>
+            ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
