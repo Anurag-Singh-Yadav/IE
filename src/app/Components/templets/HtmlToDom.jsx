5062e517e42b5ea6ddab67bcca172420b78a825a
aditya-kumar-25 <aditya.pca.25@gmail.com>
2024-02-14 16:50:12 +0530

    try

--- src/app/Components/templets/HtmlToDom.jsx
+++ src/app/Components/templets/HtmlToDom.jsx
@@ -3,9 +3,15 @@ import parse from "html-react-parser";
 
 const HtmlToDom = ({ htmlContent }) => {
   // Sanitize the HTML content
-  const cleanHTML = htmlContent
+  const cleanHTML = htmlContent;
 
-  return <div id="article-content-container">{parse(cleanHTML)}</div>;
+  return (
+    <div>
+      { htmlContent && 
+        <div id="article-content-container">{parse(cleanHTML)}</div>
+      }
+    </div>
+  );
 };
 
 export default HtmlToDom;
