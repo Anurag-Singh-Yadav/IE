51a3f5d62a1eb7c8a4837715a9f29f1e4579ffc6
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-02-21 16:33:06 +0530

    accepts

--- src/app/Components/HomeComponents/InterviewCard.jsx
+++ src/app/Components/HomeComponents/InterviewCard.jsx
@@ -1,13 +1,13 @@
 "use client";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Avatar from "react-avatar";
 import { TiTick } from "react-icons/ti";
 import { RxCrossCircled } from "react-icons/rx";
 import { GrLinkedin } from "react-icons/gr";
-import Image from "next/image";
 import Link from "next/link";
 import Cookies from "js-cookie";
 import axios from "axios";
+import Acceptinterview from "../Acceptinterview";
 function InterviewCard({
   name,
   userPhoto,
@@ -20,25 +20,25 @@ function InterviewCard({
   id,
   isAdmin,
   isClick,
-  setIsClicked
+  setIsClicked,
 }) {
-
-
-  const adminChoics = async (choics)=>{
+  const adminChoics = async (choics) => {
     const token = Cookies.get("token");
     setIsClicked(true);
     const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_ADMIN_URL}${process.env.NEXT_PUBLIC_ADMIN_TO_ACCEPTS_INTERVIEW_BLOG}`,{
-        id:id,
-        isAdminAccepted:choics
-      },{
-        headers:{
-          Authorization: `Bearer ${token}`
-        }
+      `${process.env.NEXT_PUBLIC_ADMIN_URL}${process.env.NEXT_PUBLIC_ADMIN_TO_ACCEPTS_INTERVIEW_BLOG}`,
+      {
+        id: id,
+        isAdminAccepted: choics,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
     );
     setIsClicked(false);
-  }
+  };
 
   return (
     <div className="my-3 py-4 text-black px-4 border-l-2 hover:border-green-bg shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
@@ -87,21 +87,11 @@ function InterviewCard({
             </div>
           )}
         </div>
-        
 
         <div>Published on : {created_on}</div>
       </div>
-      {isAdmin && (
-        <div className="flex justify-evenly py-2">
-          <div className={`transition-all duration-300 hover:bg-green-bg px-4 py-2 bg-light-green2 rounded-md ${isClick == true ? 'cursor-wait' : 'cursor-pointer'}`} onClick={()=>{
-            adminChoics(true)
-          }}>Accept</div>
-          <div className={`transition-all duration-300 hover:bg-green-bg px-4 py-2 bg-light-green2 rounded-md ${isClick == true ? 'cursor-wait' : 'cursor-pointer'}`} onClick={()=>{
-            adminChoics(false)
-          }}>Reject</div>
-        </div>
-      )}
-      <div className="flex justify-between my-2 items-center">
+
+      <div className="flex justify-between my-2 flex-wrap items-center">
         <Link
           href={`/interview-experience/[id]`}
           as={`/interview-experience/${id}`}
@@ -109,6 +99,8 @@ function InterviewCard({
         >
           Read
         </Link>
+        {isAdmin && (
+          <Acceptinterview adminChoics={adminChoics} isClick={isClick} />)}
         {linkedin_id && (
           <div className="flex justify-between gap-2 items-center hover:text-blue-700">
             <span className=" font-medium cursor-pointer hover:font-semibold">
---
+++ src/app/admin/interview-experience/page.js
@@ -0,0 +1,68 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import WebsiteBanner from "@/app/Components/templets/WebsiteBanner";
+import InterviewCard from "@/app/Components/HomeComponents/InterviewCard";
+import axios from "axios";
+function Page() {
+  const [interviewData, setInterviewData] = useState(null);
+  const [isClick,setIsClicked] = useState(false);
+
+  const fetchInterviews = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BASE_URL}${
+          process.env.NEXT_PUBLIC_GET_INTERVIEW_EXPERIENCE
+        }/${true}`
+      );
+      setInterviewData(response.data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchInterviews();
+  }, [isClick]);
+  return (
+    <div>
+      <div>
+        <WebsiteBanner
+          heading="This is Admin section to accept the Interview Experience"
+          BtnName="Read More"
+          imgSrc="interview-Experience.svg"
+          paragraph="Interview Experience is a platform where you can find interview experiences of top companies. We have a collection of interview experiences of top companies like Google, Amazon, Microsoft, etc. You can also share your interview experience with us."
+        ></WebsiteBanner>
+      </div>
+      <div className="bg-[url('/dsaPrac.svg')] pt-4 pb-8 main-container">
+        <div className="font-semibold sm:py-4 md:py-8 text-lg sm:text-xl md:text-2xl lg:text-3xl">
+          Explore all{" "}
+          <span className="text-green-bg underline">Interview Experience</span>
+        </div>
+
+        <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
+          {interviewData &&
+            interviewData.map((interview, index) => (
+              <InterviewCard
+                key={index}
+                name={interview.name}
+                userPhoto={interview.avatar}
+                company={interview.company}
+                linkedin_id={interview.linkedin_id}
+                created_on={interview.created_on}
+                selected={interview.selected}
+                position={interview.position}
+                round={interview.round}
+                isClick={isClick}
+                setIsClicked={setIsClicked}
+                company_logo={interview.company_logo}
+                id={interview._id}
+                isAdmin={true}
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page;
