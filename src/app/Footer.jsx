bab6bafd7105b39a212cc0343ad44d0cbb7fd191
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-02-22 20:26:26 +0530

    subscribe

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -5,13 +5,15 @@ import { FaTwitter } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
 import "./Footer.css";
+import Cookies from "js-cookie";
+import { getSubscribe, getSubscription } from "./fetchDetails/subscribe";
 
 function Footer() {
   const logo = "<IE/>";
-
+  const [subscribed, setSubscribed] = useState(false);
   const updateFooterClass = () => {
     const footerElement = document.querySelector("#f-top");
-    const isSmallScreen = window.innerWidth <= 640; // You can adjust the breakpoint as needed
+    const isSmallScreen = window.innerWidth <= 640;
 
     if (isSmallScreen) {
       footerElement.classList.remove("footer-top");
@@ -28,37 +30,62 @@ function Footer() {
     return () => {
       window.removeEventListener("resize", updateFooterClass);
     };
-  } , [])
+  }, []);
 
-  const [userEmail, setUserEmail] = useState("");
+  useEffect(()=>{
+    clickHandler();
+  },[])
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+  const clickHandler = async () => {
+    const token = Cookies.get("token");
+    if (!token) {
+      alert("Please login to subscribe");
+      setSubscribed(false);
+      return;
+    }
+    const response = await getSubscribe(token);
+    if (response === "error") {
+      alert("Some error occured. Please try again later.");
+      setSubscribed(false);
+      return;
+    }
+    if (response === "invalid token") {
+      alert("Please login to subscribe");
+      setSubscribed(false);
+      return;
+    }
 
-    // subscribe 'userEmail'
-
-    setUserEmail("");
+    if(response.subscribed === true){
+      setSubscribed(true);
+      return;
+    }else{
+      setSubscribed(false);
+    }
   };
 
   return (
     <div className="relative footer-container ">
-
-
-      <div className={`grid grid-cols-1 gap-x-3 gap-y-7 md:grid-cols-6 items-start text-sm py-2 sm:py-3 md:py-6 text-center`}>
-
-        <div id="f-top" className={`footer-top background-grid flex justify-around flex-wrap items-center bg-white text-black overflow-hidden w-[94vw] sm:w-[88vw] md:w-[87vw] lg:w-[80vw] rounded-3xl px-7 sm:rounded-full shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]`}>
+      <div
+        className={`grid grid-cols-1 gap-x-3 gap-y-7 md:grid-cols-6 items-start text-sm py-2 sm:py-3 md:py-6 text-center`}
+      >
+        <div
+          id="f-top"
+          className={`footer-top background-grid flex justify-around flex-wrap items-center bg-white text-black overflow-hidden w-[94vw] sm:w-[88vw] md:w-[87vw] lg:w-[80vw] rounded-3xl px-7 sm:rounded-full shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]`}
+        >
           <div className="absolute w-[50%] right-0 h-full right-gradient " />
 
           <div className="absolute w-[50%] left-0 h-full left-gradient" />
 
           <div className="absolute w-full left-0 h-full down-gradient" />
           <div className="z-[1]">
-            <p className="text-green-bg text-lg font-semibold">Ready to get started?</p>
+            <p className="text-green-bg text-lg font-semibold">
+              Ready to get started?
+            </p>
             <p className="text-dark-blue">Check out out courses today.</p>
           </div>
 
           <button className="font-bold text-base  md:text-lg z-[1] start px-6 py-3">
-            Get Started {'-->'}
+            Get Started {"-->"}
           </button>
         </div>
 
@@ -90,61 +117,62 @@ function Footer() {
             </p>
             <div className="flex flex-col gap-2">
               <p>Subscribe to get important updates on your mail.</p>
-              <form onSubmit={submitHandler}>
-                <input
-                  type="email"
-                  value={userEmail}
-                  placeholder="Enter email"
-                  onChange={(e) => setUserEmail(e.target.value)}
-                  className="p-2 rounded-md text-black"
-                />
-                <input
-                  type="submit"
-                  value="Subscribe"
-                  className=" cursor-pointer btn-gradient-2 p-2 rounded m-2"
-                />
-              </form>
+              {!subscribed && (
+                <div
+                  className="py-2 px-4 start-2 text-black font-semibold rounded-md transition duration-300 cursor-pointer"
+                  onClick={() => {
+                    clickHandler();
+                  }}
+                >
+                  Subscribe
+                </div>
+              )}
+
+              {subscribed && (
+                <div className="py-2 px-4 bg-red-500 text-black font-semibold rounded-md transition duration-300 cursor-pointer">
+                  UnSubscribed
+                </div>
+              )}
+
             </div>
           </div>
         </div>
-        
       </div>
 
-
       <div className="flex flex-col items-center gap-4 py-3">
-          <ul className="wrapper text-black grid sm:grid-cols-3 md:grid-cols-5 grid-cols-2"> 
-            <li className="icon facebook">
-              <span className="tooltip">Facebook</span>
-              <span>
-                <FaFacebook className="fab fa-facebook-f"></FaFacebook>
-              </span>
-            </li>
-            <li className="icon twitter">
-              <span className="tooltip">Twitter</span>
-              <span>
-                <FaTwitter className="fab fa-twitter"></FaTwitter>
-              </span>
-            </li>
-            <li className="icon instagram">
-              <span className="tooltip">Instagram</span>
-              <span>
-                <FaSquareInstagram className="fab fa-instagram"></FaSquareInstagram>
-              </span>
-            </li>
-            
-            <li className="icon youtube">
-              <span className="tooltip">Youtube</span>
-              <span>
-                <FaYoutube className="fab fa-youtube"></FaYoutube>
-              </span>
-            </li>
-          </ul>
-          <p className="text-sm tracking-wide">
-            {" "}
-            Copyright © InterviewExpress <span id="year"></span> | All rights
-            reserved
-          </p>
-        </div>
+        <ul className="wrapper text-black grid sm:grid-cols-3 md:grid-cols-5 grid-cols-2">
+          <li className="icon facebook">
+            <span className="tooltip">Facebook</span>
+            <span>
+              <FaFacebook className="fab fa-facebook-f"></FaFacebook>
+            </span>
+          </li>
+          <li className="icon twitter">
+            <span className="tooltip">Twitter</span>
+            <span>
+              <FaTwitter className="fab fa-twitter"></FaTwitter>
+            </span>
+          </li>
+          <li className="icon instagram">
+            <span className="tooltip">Instagram</span>
+            <span>
+              <FaSquareInstagram className="fab fa-instagram"></FaSquareInstagram>
+            </span>
+          </li>
+
+          <li className="icon youtube">
+            <span className="tooltip">Youtube</span>
+            <span>
+              <FaYoutube className="fab fa-youtube"></FaYoutube>
+            </span>
+          </li>
+        </ul>
+        <p className="text-sm tracking-wide">
+          {" "}
+          Copyright © InterviewExpress <span id="year"></span> | All rights
+          reserved
+        </p>
+      </div>
     </div>
   );
 }
