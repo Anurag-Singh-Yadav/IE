189d4482791f4824c97e31ac1018bc8b3936fdd2
aditya-kumar-25 <aditya.pca.25@gmail.com>
2024-04-05 17:55:42 +0530

    asdfghjkl;

---
+++ src/app/admin/add-article/page.js
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react';
+import AddContent from '../AddContent';
+
+export default function App() {
+  return (
+    <div> 
+      <AddContent />
+    </div>
+  );
+}
