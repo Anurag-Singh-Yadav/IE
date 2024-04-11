a257a7cff276f33d0806897e19befd7128615d7d
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-04-12 00:53:49 +0530

    correct

--- src/app/dashboard/what-to-render/UserData.jsx
+++ src/app/dashboard/what-to-render/UserData.jsx
@@ -4,6 +4,7 @@ import ApexChart from "../dashboardComponents/CircularRadial";
 import LeaderboardPercentageChart from "../dashboardComponents/LeaderboardPercentageChart";
 import DashboardCards from "../dashboardComponents/DashboardCards";
 import { GiFlame } from "react-icons/gi";
+import ChallengeProgress from "../dashboardComponents/ChallengeProgress";
 
 function UserData() {
   const [progressBarData, setProgressBarData] = useState([50, 70, 120]);
@@ -11,7 +12,7 @@ function UserData() {
   const [currentStreak, setCurrentStreak] = useState(5);
 
   return (
-    <div className="flex flex-col gap-12">
+    <div className="flex flex-col gap-12 h-[300vh]">
       <p className="font-bold text-2xl">{"Dashboard"}</p>
 
       <DashboardCards followers={30} following={45} profileViews={32} />
@@ -25,15 +26,41 @@ function UserData() {
         </div>
         <div className="col-span-2 bg-white box-shadow">
           <p>Streak</p>
+          <div className='flex justify-start items-start'>
           <p>
             {currentStreak}
             {" Days"}
           </p>
           <GiFlame />
+          </div>
         </div>
+      </div>
+
+      <div>
+        <ChallengeProgress></ChallengeProgress>
       </div>
     </div>
   );
 }
+
+const announcement = [
+  {
+    title: "New Feature",
+    description: "We have added a new feature to the platform",
+  },
+  {
+    title: "New Feature",
+    description: "We have added a new feature to the platform",
+  },
+  {
+    title: "New Feature",
+    description: "We have added a new feature to the platform",
+  },
+  {
+    title: "New Feature",
+    description: "We have added a new feature to the platform",
+  },
+];
+
 
 export default UserData;
