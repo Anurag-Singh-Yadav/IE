55e6583c76ceec18764360a92a59a42392c855c8
aditya-kumar-25 <aditya.pca.25@gmail.com>
2024-04-29 01:19:24 +0530

    sfajdaskj

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,27 +1,11 @@
-'use client'
-import React, { useEffect } from 'react'
-import {useSelector} from 'react-redux'
-import {useRouter} from 'next/navigation' 
-
+import React from 'react'
 
 function page() {
-  const router = useRouter();
-  const {userHandle} = useSelector((state) => state.GlobalState.userDetails);
 
-  useEffect(() => {
-    if(userHandle){
-    router.push(`/dashboard/${userHandle}`);
-    window.location.reload();
-    }
-    else{
-      alert('Please login first');
-      router.push('/');
-      window.location.reload();
-    }
-  } , [userHandle])
 
   return (
     <div>
+        NOT FOUND !!!
     </div>
   )
 }
